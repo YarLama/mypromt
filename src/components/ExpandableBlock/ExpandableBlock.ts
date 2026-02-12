08f98654043bf2d59ea603c0587f4a17064ababa
YarLama <yarlamas@gmail.com>
2026-02-13 00:53:22 +0300

    wip: basic functional open/close

--- src/components/ExpandableBlock/ExpandableBlock.ts
+++ src/components/ExpandableBlock/ExpandableBlock.ts
@@ -1,33 +1,50 @@
 import styles from './style.css?inline'
 
 export class ExpandableBlock extends HTMLElement {
- 
+
   private shadow: ShadowRoot;
+  private isOpen = true;
 
   constructor() {
     super()
-    this.shadow = this.attachShadow({ mode: 'open'});
+    this.shadow = this.attachShadow({ mode: 'open' });
   }
 
   connectedCallback() {
     this.render();
+    this.registerListeners();
   }
 
   disconnectedCallback() {
     // remove listeners, e.t.c
   }
 
+  private registerListeners() {
+    const header = this.shadowRoot?.querySelector('.header')
+    const content = this.shadowRoot?.querySelector('.content')
+    const arrow = this.shadowRoot?.querySelector('.arrow')
+
+    header?.addEventListener('click', () => {
+      content?.classList.toggle('hidden');
+      this.isOpen = !this.isOpen;
+      if (arrow) arrow.textContent = this.isOpen ? '▼' : '▶';
+    })
+  }
+
   private render() {
+    const label = this.getAttribute('label') || ''
     this.shadow.innerHTML = `
       <style>${styles}</style>
       <div>
-        <div>Test ExpandableBlock</div>
-        <!-- future web components childs -->
-        <div class="test_content">
+        <div class="header">
+        ${label && `<div>${label}</div>`}
+        <div class="arrow">▼</div>
+        </div>
+        <div class="content">
           <slot></slot>
         </div>
       </div>
-    `
+    `;
   }
 }
 
