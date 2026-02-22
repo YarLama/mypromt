0e411df31ddc699756288cbf053aa00ed8c10823
YarLama <yarlamas@gmail.com>
2026-02-22 04:08:16 +0300

    wip: extend button functions

--- src/utils/actionLogic.ts
+++ src/utils/actionLogic.ts
@@ -1,10 +1,16 @@
+import { constitution } from "../store/constitution";
 import { appEvents, type appEventsType } from "./types/actions";
 
 
 type VoidHandler = () => void;
 
+const getConstitutionHandler = () => {
+  const text = constitution.getFullText();
+  navigator.clipboard.writeText(text);
+}
+
 export const AppHandlers: Record<appEventsType, VoidHandler> = {
- [appEvents.getConstitution]: () => { console.log('getConstitution')},
+ [appEvents.getConstitution]: () => getConstitutionHandler(),
  [appEvents.getTask]: () => { console.log('getTask') },
  [appEvents.openModal]: () => { console.log('openModal') },
  [appEvents.closeModal]: () => {},
