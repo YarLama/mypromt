a66a4b8711760fd3f32de43fb71c51d04850bb26
YarLama <yarlamas@gmail.com>
2026-02-24 08:10:18 +0300

    refactor: Expandable component

--- src/components/ExpandableBlock/ExpandableBlock.ts
+++ src/components/ExpandableBlock/ExpandableBlock.ts
@@ -3,7 +3,7 @@ import styles from './style.css?inline'
 export class ExpandableBlock extends HTMLElement {
 
   private shadow: ShadowRoot;
-  private isOpen = true;
+  private isOpen: boolean = (this.getAttribute('expanded') ?? false) === false;
 
   constructor() {
     super()
@@ -19,24 +19,28 @@ export class ExpandableBlock extends HTMLElement {
     const header = this.shadowRoot?.querySelector('.header')
     const content = this.shadowRoot?.querySelector('.content')
     const arrow = this.shadowRoot?.querySelector('.arrow')
-
+    
     header?.addEventListener('click', () => {
       content?.classList.toggle('hidden');
       this.isOpen = !this.isOpen;
       if (arrow) arrow.textContent = this.isOpen ? '▼' : '▶';
     })
+
+    
   }
 
   private render() {
     const label = this.getAttribute('label') || ''
+    const arrow = this.isOpen ? '▼' : '▶';
+    const contentClass = this.isOpen ? "content" : "content hidden";
     this.shadow.innerHTML = `
       <style>${styles}</style>
       <div class="expand">
         <div class="header">
           ${label && `<div>${label}</div>`}
-          <div class="arrow">▼</div>
+          <div class="arrow">${arrow}</div>
         </div>
-        <div class="content">
+        <div class="${contentClass}">
           <slot></slot>
         </div>
       </div>
