ebeb4598c9eb5c3136489653dda1b9b303f0ec41
YarLama <yarlamas@gmail.com>
2026-02-26 09:01:58 +0300

    feat: InputTask

--- src/components/InputTask/InputTask.ts
+++ src/components/InputTask/InputTask.ts
@@ -5,6 +5,7 @@ export class InputTask extends HTMLElement {
 
   private shadow: ShadowRoot;
   private _state: 'mark' | 'edit' = 'edit';
+  private _taskText = "";
   private _markList: Map<string, string> = marksDefault;
   private _selectedMark: string = this._markList.keys().next().value || "";
 
@@ -13,43 +14,83 @@ export class InputTask extends HTMLElement {
     this.shadow = this.attachShadow({ mode: 'open' });
   }
 
+  get value() {
+    const value = {
+      "mark": `[${this._selectedMark}]`,
+      "text": this._taskText
+    }
+    return value;
+  }
+
   connectedCallback() {
+    this.updateRender();
+  }
+
+  private updateRender() {
     this.render();
     this.registerListeners();
-    console.log(this._selectedMark)
   }
 
   private registerListeners() {
     const removeEl = this.shadowRoot?.querySelector('.remove');
+    const toMarkEl = this.shadowRoot?.querySelector('.to-mark-layer');
+    const markLayerEl = this.shadowRoot?.querySelector('.mark-layer');
 
     if (removeEl) {
       removeEl.addEventListener('click', () => this.remove());
     }
+
+    if (markLayerEl) {
+      markLayerEl.addEventListener('click', (e) => {
+        const target = e.target as HTMLElement;
+        const markEl = target.closest('.mark');
+        const toEditEl = target.closest('.to-edit-layer');
+        
+        if (markEl instanceof HTMLElement) {
+          this._selectedMark = markEl.dataset.mark || "";
+          console.log(this._selectedMark)
+          this._state = "edit";
+          this.updateRender();
+          return;
+        }
+
+        if (toEditEl) {
+          this._state = "edit";
+          this.updateRender();
+        }
+      })
+    }
+
+    if (toMarkEl) {
+      toMarkEl.addEventListener('click', () => {
+        const inputText = this.shadowRoot?.querySelector('.input')?.textContent || "";
+        this._taskText = inputText;
+        this._state = "mark";
+        this.updateRender();
+      });
+    }
   }
 
   private render() {
-    const markElements = Array.from(this._markList.keys()).map(m => {
-      return `<div class="mark" data-mark="${m}">${m}</div>`
+    const markElements = Array.from(this._markList, ([m, d]) => {
+      return `<div class="mark" data-mark="${m}" title="${d}">${m}</div>`
     }).join("");
+    const markLayerClass = ["mark-layer", this._state !== "mark" ? "hidden" : ""].join(" ");
+    const editLayerClass = ["edit-layer", this._state !== "edit" ? "hidden" : ""].join(" ");
 
     this.shadow.innerHTML = `
       <style>${styles}</style>
       <div class="container">
-        
-        <div class="tooltip">
-          <div class="remove">x</div>
-        </div>
-
-        <div class="mark-layer">
+        <div class="${markLayerClass}">
           ${markElements}
-          <div class="to-edit-layer">edit</div>
         </div>
-
-        <div class="edit-layer">
+        <div class="${editLayerClass}">
           <div class="to-mark-layer">${this._selectedMark}</div>
-          <div contenteditable class="input"></div>
+          <div contenteditable class="input">${this._taskText}</div>
         </div>
-
+        <div class="tooltip">
+          <div class="remove">✖</div>
+        </div>
       </div>
     `
   }
