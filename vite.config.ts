8461aede9b3885f1bdb5bae4d3089c06b01f29c2
YarLama <yarlamas@gmail.com>
2026-02-28 15:30:06 +0300

    feat: My Promt Generator CI/CD

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,35 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: '/mypromt/',
   server: {
     port: 5999,
     open: true,
     host: 'localhost'
-  }
+  },
+  plugins: [
+    VitePWA({
+      strategies: "injectManifest",
+      srcDir: "src",
+      filename: "sw.ts",
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.svg"],
+      injectManifest: {
+        swDest: 'dist/sw.js'
+      },
+      manifest: {
+        name: "My promt generator",
+        short_name: "My promt",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: 'web-app-manifest-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          }
+        ]
+      },
+    })
+  ]
 })
